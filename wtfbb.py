53b2728f7fdbe75e734ede962929600ec4d1c483
Inndy <Inndy.mail@gmail.com>
2014-12-04 17:22:53 +0800

    Full content

--- wtfbb.py
+++ wtfbb.py
@@ -87,9 +87,8 @@ response = session.post(("https://elearning.ntust.edu.tw/webapps/streamViewer/"
                         proxies = Proxies, verify = False)
 
 data = response.json()["sv_streamEntries"]
-data = [{"context": obj["se_context"],
-         "course_id": obj["se_courseId"] if obj["se_filterName"] else "general"
-        } for obj in data ]
 for obj in data:
+    obj["context"] = obj["se_context"]
+    obj["course_id"] = obj["se_courseId"] if obj["se_filterName"] else "general"
     obj["course_name"] = get_course_name(obj["course_id"])
 open("output.json", "w").write(json.dumps(data, indent = 2))
